52623d3482c2a21616ac3c9b92f70efa1c872e4a
niki <nikiali004@gmail.com>
2025-08-24 22:05:41 +0330

    urls.py added

--- signup_project/urls.py
+++ signup_project/urls.py
@@ -1,22 +1,21 @@
-"""
-URL configuration for signup_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+# accounts/urls.py
+from django.urls import path, include
+from . import views
+from django.contrib.auth import views as auth_views # Views of Django Identity
+from .views import CustomLoginView, CustomRegisterView
+app_name = 'accounts'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path('register/', views.register_view, name='register'), 
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('book-now/', views.book_now_view, name='book_now'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('login-register/', views.login_register_view, name='login_register'),
+
+]   
+   
